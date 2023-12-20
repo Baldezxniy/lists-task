f422ecb9d4346fbdf59acb41fe18e16cb2f1f908
Мирослав Косюк <87585769+Baldezxniy@users.noreply.github.com>
2023-12-20 12:36:59 +0200

    #20 fixed failed test

--- src/test/java/com/example/tasklist/TaskListApplicationTests.java
+++ src/test/java/com/example/tasklist/TaskListApplicationTests.java
@@ -3,11 +3,12 @@ package com.example.tasklist;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class TaskListApplicationTests {
 
   @Test
   void contextLoads() {
   }
-
 }
